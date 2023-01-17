4bcca0b1a8fffc6f38e84a2e71cf5a9002b2448b
deepak7691 <119676343+deepak7691@users.noreply.github.com>
2023-01-18 00:12:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,19 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+const [color, setColor] = useState('redColor');
+  const toggle =()=>{
+    if(color === 'redColor'){
+      setColor('blueColor')
+    }
+    else{
+      setColor('redColor')
+    }
+  }
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={color} >Newton School</p>
+      <button id='button' onClick={toggle}>Change Style</button>
     </div>
   )
 }
